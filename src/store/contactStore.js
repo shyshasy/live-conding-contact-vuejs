7737baed450ee5854115fad4cb9ea4accdb83c8a
CoachFofana <bfofana.ext@simplon.co>
2024-08-26 12:34:13 +0200

    Add functions in contactStore

--- src/store/contactStore.js
+++ src/store/contactStore.js
@@ -4,6 +4,7 @@ const useContactStore = defineStore('contactStore', {
     state: () => (
         {
             contacts: [],
+            currentContact: null,
             id: 0
         }
     ),
@@ -15,6 +16,27 @@ const useContactStore = defineStore('contactStore', {
                     ...contact
                 }
             )
+        },
+        edit(id, newContact){
+            const contactIndex = this.contacts.findIndex(c => c.id === id);
+            if(contactIndex !== -1){
+                this.contacts[contactIndex] = {
+                    ...id,
+                    newContact
+                }
+            }
+        },
+        show(id){
+            const contactIndex = this.contacts.findIndex(c => c.id === id);
+            if(contactIndex !== -1){
+                this.currentContact = this.contacts[contactIndex];
+            }
+        },
+        remove(id){
+            const contactIndex = this.contacts.findIndex(c => c.id === id);
+            if(contactIndex !== -1){
+               this.contacts = this.contacts.filter(c => c.id !== id);
+            }
         }
     },
 })
